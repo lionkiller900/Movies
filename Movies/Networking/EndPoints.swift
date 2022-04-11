b6c29cd1584b9d164b772a20ccc5ed617f84c127
lionkiller900 <donalddaniels20@hotmail.com>
2022-04-11 11:42:00 +0100

    created endpoints parameters for the url in the Endpoints file

--- Movies/Networking/EndPoints.swift
+++ Movies/Networking/EndPoints.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+//https://api.themoviedb.org/3/search/movie?api_key=3215a185b25eb297a66e63d137fb994f&language=en-US&query=Star
+
+//https://image.tmdb.org/t/p/w300/lV5OpzAss1z06YNagOVap1I35mH.jpg?api_key=3215a185b25eb297a66e63d137fb994f
+
+struct EndPoint {
+    static let baseUrl = "https://api.themoviedbccc.org/3/"
+    static let imagesBaseUrl = "https://image.tmdb.org/t/p/w300/"
+}
+
+struct Path {
+    static let movies = "search/movie/"
+}
