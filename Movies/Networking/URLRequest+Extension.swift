d5a3ee1a41037548aa8d4d534487914b2e16898b
lionkiller900 <donalddaniels20@hotmail.com>
2022-04-11 11:26:57 +0100

    created an extension for URL api request

--- Movies/Networking/URLRequest+Extension.swift
+++ Movies/Networking/URLRequest+Extension.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+extension URLRequest {
+    static func getURLRequest(for apiRequest:ApiRequestType)-> URLRequest? {
+        if let url = URL(string:apiRequest.baseUrl.appending(apiRequest.path)),
+           var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false){
+            let queryItems = apiRequest.params.map { key, value in
+                URLQueryItem(name: key, value: value)
+            }
+            urlComponents.queryItems = queryItems
+            let urlRequest = URLRequest(url: urlComponents.url!)
+            return urlRequest
+        } else {
+            return nil
+        }
+    }
+}
