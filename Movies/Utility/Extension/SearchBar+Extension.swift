f6064d44dc8cf44785a6be3e214217527713d9e4
lionkiller900 <donalddaniels20@hotmail.com>
2022-04-11 11:15:52 +0100

    added an extension for the search field

--- Movies/Utility/Extension/SearchBar+Extension.swift
+++ Movies/Utility/Extension/SearchBar+Extension.swift
@@ -6,4 +6,15 @@
 //
 
 import Foundation
+import Combine
+import UIKit
 
+extension UITextField {
+    var textPublisher: AnyPublisher<String, Never> {
+        NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification, object: self)
+            .compactMap { $0.object as? UITextField } // receiving notifications with objects which are instances of UITextFields
+            .compactMap(\.text) // extracting text and removing optional values (even though the text cannot be nil)
+            .eraseToAnyPublisher()
+    }
+}
