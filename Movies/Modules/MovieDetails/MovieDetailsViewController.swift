71e91d0ae60d36c5e93a1e82cec2036c3e32cceb
lionkiller900 <donalddaniels20@hotmail.com>
2022-04-11 10:07:10 +0100

    linked the storyboard to my viewcontroller included their label functionality

---
+++ Movies/Modules/MovieDetails/MovieDetailsViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MovieDetailsViewController.swift
+//  Movies
+//
+//  Created by Admin on 11/04/2022.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var reviewsLbl: UILabel!
+    
+    @IBOutlet weak var descLbl: UILabel!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    var viewModel:MovieDetailsViewModel?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        setupUI()
+    }
+    
+    func setupUI() {
+        guard let viewModel = viewModel else {
+            return
+        }
+ 
+        titleLbl.text = viewModel.movie.title
+        descLbl.text = viewModel.movie.overView
+        reviewsLbl.text = "\(viewModel.movie.reviews)"
+        let url = URL(string:"\(EndPoint.imagesBaseUrl)\(viewModel.movie.poster)")
+        posterImageView.kf.setImage(with: url)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
