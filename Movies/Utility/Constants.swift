7ebdf591972bab44902bb95f33126cbf9bb326ec
lionkiller900 <donalddaniels20@hotmail.com>
2022-04-11 10:47:27 +0100

    created both files for reachability and constants for movie utility

---
+++ Movies/Utility/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  Movies
+//
+//  Created by Admin on 11/04/2022.
+//
+
+import Foundation
---
+++ Movies/Utility/Rechability.swift
@@ -0,0 +1,9 @@
+//
+//  Rechability.swift
+//  Movies
+//
+//  Created by Admin on 11/04/2022.
+//
+
+import Foundation
+import SystemConfiguration
