e8ca9a236bf502e6ecfda9829e408b89bf746ddb
lionkiller900 <donalddaniels20@hotmail.com>
2022-04-11 11:31:12 +0100

    added parameters for API Request in the APIRequest

--- Movies/Networking/ApiRequest.swift
+++ Movies/Networking/ApiRequest.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+protocol ApiRequestType {
+    var baseUrl:String {get}
+    var path:String {get}
+    var params:[String:String] {get}
+}
+
+struct ApiRequest:ApiRequestType {
+    var baseUrl: String
+    var path: String
+    var params: [String : String]
+}
