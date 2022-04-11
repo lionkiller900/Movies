dade1633e195016dc35840b6646cce2bbb99915c
lionkiller900 <donalddaniels20@hotmail.com>
2022-04-11 11:17:50 +0100

    added a functionality for the alert Controller

--- Movies/Utility/Extension/Controller+Extension.swift
+++ Movies/Utility/Extension/Controller+Extension.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController {
+    func showAlert(message:String) {
+        let alertViewController = UIAlertController(title:"Message", message:message, preferredStyle: UIAlertController.Style.alert)
+        let alertAction = UIAlertAction(title:"Ok", style: UIAlertAction.Style.cancel, handler: { (alert) in
+            alertViewController.dismiss(animated:true, completion:nil)
+        })
+        alertViewController.addAction(alertAction)
+        self.present(alertViewController, animated:true, completion:nil)
+    }
+}
